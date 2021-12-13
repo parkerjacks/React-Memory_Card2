879eae531b4722cc7702a0034775740fc304d1a2
Parker J <parkerj@Parkers-iMac.attlocal.net>
2021-12-12 21:40:06 -0500

    started code and created memory card component and css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Memory_Card from './components/MemoryCard'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> Memory Game </h1>
+        <h2> Match cards to win </h2>
       </header>
+
+      <Memory_Card> </Memory_Card>
     </div>
   );
 }
---
+++ src/components/MemoryCard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './MemoryCard.css'
+
+class Memory_Card extends React.Component { 
+    
+    render(){ 
+
+        return ( 
+
+        <div className="Memory_Card"> </div>
+
+        )
+    }
+
+
+}
+
+export default Memory_Card
